7cdd96a889f81b0a733a578a8045f78cc8b34c37
Hiren Garai <47427546+hirengarai@users.noreply.github.com>
2023-12-26 07:35:55 +0530

    Add files via upload

---
+++ blcokfind.py
@@ -0,0 +1,96 @@
+"""
+ * REFERENCE IMPLEMENTATION OF block searching programe.
+ *
+ * Filename: blockfind.py
+ *
+ * created: 07/12/23
+ * updated: 20/12/23
+ *
+ * by Hiren
+ * Research Scholar
+ * BITS Pilani, Hyderabad Campus
+ *
+ * Synopsis:
+ * This script finds the consecutive PNBs
+ * running command: python3/python blockfind.py
+"""
+
+
+listBLOCKs =[] # list form
+
+
+list = [2, 3, 4, 5, 6, 39, 47, 48, 66, 67, 68, 69, 70, 71, 72, 73, 74, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 90, 91, 92, 95, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 115, 123, 124, 125, 126, 127, 155, 156, 157, 158, 159, 168, 191, 192, 193, 194, 199, 200, 201, 207, 208, 211, 212, 213, 219, 220, 221, 222, 223, 224, 225, 226, 227, 244, 245, 246, 255] # PNBs
+
+# print(f"The initial length of the list: {len(list)}")
+
+block_len = 5 # how many consecutive elements to find
+
+if block_len > len(list):
+    print("Block length is too high")
+
+else:
+    list.sort()
+    i = 0   
+    while i < len(list):
+        counter = 1
+        block = [list[i]]
+        i += 1
+        
+        while i < len(list) and list[i] - list[i-1] == 1 and counter < block_len:
+            block.append(list[i])
+            # print(block)
+            counter+=1
+            i += 1
+            
+        if (counter == block_len):
+            # BLOCKs.append(block[:block_len])
+            for j in range(block_len):
+                listBLOCKs.append(block[j])
+# print(BLOCKs)
+# print( len(listBLOCKs)," elements arranged in consecutive", block_len, " block size: ")
+# print(listBLOCKs)
+
+rest = []
+for i in list:
+    if i not in listBLOCKs:
+        rest.append(i)
+        
+# print("The rest of the ", len(rest)," elements are: ")
+# print(rest)  
+print(*listBLOCKs, sep=" ", file = open('europnbblock.txt','w'))  
+
+
+with open('europnbblock.txt', 'a') as file:
+    for i in rest:
+        file.write(f"{i} ")
+    file.write(f"{ len(listBLOCKs)} ")
+    file.write(f"{len(rest)} ")
+    
+
+if len(rest)+len(listBLOCKs)== len(list):
+    print("Ok")
+
+# print("The last elements of the block are: {", end ="")
+
+# for i in lastelement:
+#     print(f"{i}, ", end ="")
+# print("}")
+
+# counter1=0
+# print("The first", block_len-1, "elements of the block are: {", end ="")
+# for elements in rest_cons_element:
+#     for i in elements:
+#         print(f"{i}, ", end ="")
+#         counter1+=1
+# print("}")
+
+# counter = 0
+# print("The rest of the elements are: {", end ="")
+# for i in range(256):
+#     if i not in BLOCK:
+#             counter +=1
+#             print(f"{i}, ", end ="")
+# print("}")
+
+# print(f"Total elements = {len(lastelement)+counter1+counter}")
+# print(f"\n# of blocks: {len(BLOCK)}") 
---
+++ readme.md
@@ -0,0 +1,4 @@
+To find the backaward bias of a set of PNBs in the programme the following steps are to be followed:
+1. Run the PNB programe and save the PNBs without comma in a .txt file. The last element should be the size of the list of elements.
+2. Now input the file, in the cbwbias.cpp and run the programme.
+3. If you want to use the block mode, then run the python script to find the blcoks and input that .txt file in the programme.
